f2d87e931cad5263cb7aa0f2640a68880e2ac871
Кирилл Крылов <kirillmac@MacBook-Air-Kirill.local>
2019-11-06 23:05:05 +0300

    vievs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,39 @@
 class UsersController < ApplicationController
+
   def index
-    @users = [
-    User.new(
-      id: 1,
-      name: 'Vadim',
-      username: 'installero',
-      avatar_url: 'https://secure.gravatar.com/avatar/' \
-        '71269686e0f757ddb4f73614f43ae445?s=100'
-    ),
-    User.new(id: 2, name: 'Misha', username: 'aristofun')
-  ]
+    @users = User.all
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    # Делаем проверку
+    if @user.save
+      # Если удачно, отправляем пользователя на главную с помощью метода redirect_to
+      # с сообщением
+      redirect_to root_url, notice: 'Пользователь успешно зарегистрирован!'
+    end
   end
 
   def edit
   end
 
   def show
-    @user = User.new(
-      name: 'Vadim',
-      username: 'insallero',
-      avatar_url: 'https://secure.gravatar.com/avatar/71269686e0f757ddb4f73614f43ae445?s=100'
-    )
+    @user = User.find(params[:id])
+    @new_question = @user.questions.build
+    @questions = @user.questions.to_a.select(&:persisted?)
 
-    @questions = [
-      Question.new(text: 'Как дела?', created_at: Date.parse('16.10.2019')),
-      Question.new(text: 'В чем смысл жизни?', created_at: Date.parse('16.10.2019'))
-    ]
+  end
 
-    @new_question = Question.new
-
+  private
+  def user_params
+  # берём объект params, потребуем у него иметь ключ
+  # :user, у него с помощью метода permit разрешаем
+  # набор инпутов. Ничего лишнего, кроме них, в пользователя не попадёт
+    params.require(:user).permit(:email, :password, :password_confirmation, :name, :username, :avatar_url)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,25 @@
 module ApplicationHelper
   # Этот метод возвращает ссылку на аватарку пользователя, если она у него есть.
   # Или ссылку на дефолтную аватарку, которую положим в app/assets/images
+  def declension(number, one, few, many)
+    if (11..14).include?(number % 100)
+      return many
+    end
+
+    remainder = number % 10
+    if (remainder == 1)
+      return one
+    end
+
+    if (remainder >= 2 && remainder <= 4)
+      return few
+    end
+
+    if (remainder >= 5 && remainder <= 9 || remainder == 0)
+      return many
+    end
+  end
+
   def user_avatar(user)
     if user.avatar_url.present?
       user.avatar_url
